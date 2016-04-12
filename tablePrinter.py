ec6af916ee70fa7153f580760c2044d2d9c2ef7c
Kristian Sneskov <kristian@sneskov.dk>
2016-04-12 14:51:03 +0200

    add rjustify using list comprehension

--- tablePrinter.py
+++ tablePrinter.py
@@ -10,7 +10,14 @@ def printTable(table):
     for j in range(len(table[i])):
       if len(table[i][j]) > colWidths[i]:
         colWidths[i]=len(table[i][j])
+  lst3 = []
+  for i in range(4):
+    lst3.append([item[i] for item in table])
+#  print(lst3)
   print(colWidths)
-  print(tableData)
+  for i in range(len(lst3)):
+    print('')
+    for j in range(len(lst3[i])):
+      print(lst3[i][j].rjust(colWidths[j]),end=' ')
 
 printTable(tableData)
